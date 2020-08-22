e1d570403564d7f6aa917c777564d5a3a4986cda
abhinavgoyal13 <abhinavgoyal13@gmail.com>
2020-08-22 12:51:41 +0530

    Fibonacci code added for Nth term and Limit

---
+++ Java/src/com/company/Fibonacci.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Fibonacci {
+
+    public static void fibonacciByLimit(int x) {
+        int x1 = 0;
+        int x2 = 1;
+        int sum = 0;
+        System.out.println(x1);
+        while (sum <= x) {
+            System.out.println(x2);
+            sum = x1 + x2;
+            x1 = x2;
+            x2 = sum;
+
+        }
+    }
+    public static void fibonacciByNth(int x)
+    {
+       int x1=0,x2=1,sum=0,i=1;
+        System.out.println(x1);
+       while(i<=x)
+       {
+           System.out.println(x2);
+           sum= x1+x2;
+           x1=x2;
+           x2=sum;
+           i++;
+
+       }
+
+    }
+}
--- Java/src/com/company/Main.java
+++ Java/src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Abhinav Goyal");
+        //Fibonacci
+      Fibonacci.fibonacciByLimit(100);
+      Fibonacci.fibonacciByNth(5);
     }
 }
