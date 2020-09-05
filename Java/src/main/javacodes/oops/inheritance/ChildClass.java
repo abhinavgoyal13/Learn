ea9e07d54db82519e585ca39728a2319a87823c6
abhinavgoyal13 <abhinavgoyal13@gmail.com>
2020-09-06 00:17:07 +0530

    a

--- Java/src/main/javacodes/oops/inheritance/ChildClass.java
+++
@@ -1,9 +0,0 @@
-package javacodes.oops.inheritance;
-
-public class ChildClass extends ParentClass {
-
-    @Override
-    public int sum(int x, int y) {
-        return super.sum(x, y);
-    }
-}
---
+++ JavaBasics/src/main/javacodes/oops/inheritance/ChildClass.java
@@ -0,0 +1,12 @@
+package javacodes.oops.inheritance;
+
+public class ChildClass extends ParentClass {
+
+
+
+    public static void main(String[] args)
+    {
+        ChildClass cd= new ChildClass();
+        System.out.println(cd.sum(20,30));
+    }
+}
